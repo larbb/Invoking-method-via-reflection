7f8b6377017ba510b806044e8f01cef287e3765c
Lewis Robbins <thefarcry118@gmail.com>
2012-08-25 16:29:12 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+I wrote this relflecion piece to build upon my Java knowledge. This example wastaken from a book and slight modified. I simply rewrote the layout with a removed class; the result is still the same and give the desrired result of Java reflecion (Which is pretty neat too).
+
+Ciations (Great book for any potenial Java APIer).
+Core Java, Volume 1 -- Fundementals (8th Edition). ISBN: 0132354764. Authored By: Gary Cornell and Cay S. Hortsmann.
+
---
+++ src/methodpointer.java
@@ -0,0 +1,71 @@
+import java.lang.reflect.*;
+
+/* Invoke method via retracting and points; Version 1.0; Lewis Robbins */
+
+ 
+/* Copyright (C) 2012 Lewis Robbins
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
+
+public class methodpointer 
+{
+	public static void main (String[] argv) throws NoSuchMethodException, SecurityException
+	{
+		
+		//get the pointers to the method
+		Method maths = Math.class.getMethod("sqrt", double.class);
+		
+		
+		//print table of the axis' 
+		printHistogram(1, 10, 10, maths);
+	}
+	
+	
+	/*prints a table with x and y values for a method 
+	 * @param lower bound value of x
+	 * @param the upper bound of x
+	 * @param n row(s) in the given table
+	 * @param f the method we're invoking */
+	
+	public static void printHistogram(double start, double end, int n1, Method invoke)
+	{
+		//print out the given method as the tables' title
+		System.out.println(invoke);
+		
+		double tabledp = (start - end /100 * 100) / n1 - 1;
+		
+		for (double xx = end; xx <= end; xx += tabledp) {
+			try {
+				double result = (double) invoke.invoke(null, xx); // Sorry had to use a cast.
+				
+				System.out.printf("%10.4f | %10.4f%n", result, xx);	
+			} 
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	/* return the value of the given square
+	 * @param x the implicit param
+	 * @return the computational value of the square */
+	
+	public static double square (double x)
+	{
+		return x * x;
+	}
+}
